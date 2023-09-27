21d6beba3cd2777b608e5c7ce27c3896104f77cd
armorbita <armorbita@gmail.com>
2023-09-27 06:16:01 -0800

    links functions are created

---
+++ src/pages/Restaurants.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+// import { Link } from 'react-router-dom';
+import '../assets/scss/HomePage/_homePage.scss'
+const HomePage = () => {
+
+    return (
+        <div className="restaurants_page">
+            <div className="custom_container"></div>
+        </div>
+    )
+}
+
+export default HomePage;
