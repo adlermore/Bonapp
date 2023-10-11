93ba02c27e2b950c649e5d41001999c929011e7a
armorbita <armorbita@gmail.com>
2023-10-11 07:09:23 -0800

    last changes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import logoImg from '../../assets/img/logo.png'
 
 const Header = ({positionRelative}) => {
     
@@ -6,13 +8,15 @@ const Header = ({positionRelative}) => {
         <header className={positionRelative ? 'page_header header_relative' :  'page_header '}>
             <div className="header_inner">
                 <div className="header_container">
-                    <a href="#/" className="logo_block">Logo</a>
+                    <Link to="/" className="logo_block"><img src={logoImg} alt="logo-img"/></Link>
                     <ul className="header_menu">
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li> 
+                        <li><Link to="/">Categories</Link></li>
+                        <li><Link to="/">Popular</Link></li>
+                        <li><Link to="/">Top</Link></li>
+                        <li><Link to="/">Blog</Link></li>
+                        <li><Link to="/">Search</Link></li> 
+                        <li><Link to="/">Location</Link></li> 
+                        <li><Link to="/">Contact</Link></li> 
                     </ul> 
                 </div>
             </div>
