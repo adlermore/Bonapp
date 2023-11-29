31e8dd28125c7528cf717a72c9891a2e8cce1e2d
armorbita <armorbita@gmail.com>
2023-11-29 07:10:16 -0800

    local commit

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,40 +1,15 @@
 
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import TimeKeeper from 'react-timekeeper';
+import React from 'react';
+// import { Link } from 'react-router-dom';
 import '../assets/scss/HomePage/_homePage.scss';
+import Reservation from '../components/Reservation/Reservation';
+import HomeReservationImg from '../assets/img/homeReservation.png';
 
 const HomePage = () => {
-    
-    const [time, setTime] = useState('12:34pm')
-    const [showTime, setShowTime] = useState(true)
 
     return (
         <div className="home_page">
-            <div className="search_section">
-                <div className="custom_container">
-                    <div className="main_search">
-                        <Link to="/">Welcome</Link>
-                        <a href="#/">Time</a>
-                        <a href="#/">Name</a>
-                        <div>
-                            {showTime &&
-                                <TimeKeeper
-                                    time={time}
-                                    onChange={(newTime) => setTime(newTime.formatted12)}
-                                    onDoneClick={() => setShowTime(false)}
-                                    switchToMinuteOnHourSelect
-                                />
-                            }
-                            <span>Time is {time}</span>
-                            {!showTime &&
-                                <button onClick={() => setShowTime(true)}>Show</button>
-                            }
-                        </div>
-                        <button className="site_btn">Reserve</button>
-                    </div>
-                </div>
-            </div>
+            <Reservation  background={HomeReservationImg} />
         </div>
     )
 }
