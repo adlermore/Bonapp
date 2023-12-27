818f39e114f40a20785c6a0575b13d7c267f71d9
armorbita <armorbita@gmail.com>
2023-12-27 17:30:01 +0400

    local commit

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,14 +1,14 @@
 
 import React from 'react';
 import '../assets/scss/HomePage/_homePage.scss';
-// import Reservation from '../components/Reservation/Reservation';
-// import HomeReservationImg from '../assets/img/homeReservation.png';
+import Reservation from '../components/Reservation/Reservation';
+import HomeReservationImg from '../assets/img/homeReservation.png';
 
 const HomePage = () => {
 
     return (
         <div className="home_page">
-            {/* <Reservation background={HomeReservationImg} /> */}
+            <Reservation background={HomeReservationImg} />
 
         </div>
     )
