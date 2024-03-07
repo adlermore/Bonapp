80a4eddc863a51b904548d3910083efcf9600d5e
armorbita <armorbita@gmail.com>
2024-03-07 17:45:49 +0400

    local commit

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -38,19 +38,24 @@ const Footer = () => {
                                 <a href="/#" className="icon-tiktok"> </a>
                                 <a href="/#" className="icon-whatsapp"> </a>
                             </div>
+                            <div className="payment_block">
+                                <div>
+                                    <span><img src={payment1} alt="paymentImg" /></span>
+                                    <span><img src={payment3} alt="paymentImg" /></span>
+                                    <span><img src={payment2} alt="paymentImg" /></span>
+                                </div>
+                            </div>
                         </li>
                     </ul>
-                    <div className="payment_block">
-                        <div>
-                            <span><img src={payment1} alt="paymentImg" /></span>
-                            <span><img src={payment3} alt="paymentImg" /></span>
-                            <span><img src={payment2} alt="paymentImg" /></span>
-                        </div>
+                    <div className='app_dwonload'>
+                        <div className='download_title'>Download our application</div>
+                        <a href="/#" className='icon-apple footer_app'>App Store</a>
+                        <a href="/#" className='icon-android footer_app'>Play Market</a>
                     </div>
                 </div>
                 <div className="footer_copytoght">
-                    
-                Copyright 2024 / Website by  <a target='blank' href="https://lightdesignstudio.am/" >Light Design Studio</a>
+
+                    Copyright 2024 / Website by  <a target='blank' href="https://lightdesignstudio.am/" >Light Design Studio</a>
                 </div>
             </div>
         </footer>
